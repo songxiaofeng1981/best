03e3511d1ba4bb51ba0cdce89af4b3fffd2963b9
s110b <111761629+s110b@users.noreply.github.com>
2023-11-23 14:06:09 +0800

    c
    c

--- process-images.sh
+++ process-images.sh
@@ -9,37 +9,18 @@ if [ ! -d "$IMAGE_DIR" ]; then
     exit 1
 fi
 
-
-
-# 重命名图片并压缩
-count=1
+# 压缩图片
 for file in "$IMAGE_DIR"/*.{jpg,jpeg,png}; do
     if [[ -f $file ]]; then
-        # 重命名逻辑
-        date=$(identify -verbose "$file" | grep "exif:DateTimeOriginal:" | awk '{print $2}' | sed 's/:/-/g' | sed 's/ //g')
-        if [ ! -z "$date" ]; then
-            formattedDate=$(date -d "$date" +'%Y%m%d')
-            randomNumber=$(shuf -i 1-1000 -n 1)
-            newName="${formattedDate}${randomNumber}-${file##*/}"
+        # 使用 squoosh-cli 压缩图片
+        squoosh-cli --webp auto "$file" -d "$IMAGE_DIR/after"
+        compressedFile="$IMAGE_DIR/after/$(basename "$file" .jpg).webp"
+        if [ -f "$compressedFile" ]; then
+            compressedSize=$(du -k "$compressedFile" | cut -f1)
+            echo "Compressed: $(basename "$compressedFile"), Size: ${compressedSize}KB"
         else
-            newName=$(printf "%04d-%s" $count "${file##*/}")
-            ((count++))
+            echo "Error: Compressed file not found - $compressedFile"
         fi
-        newFilePath="$(dirname "$file")/$newName"
-        mv "$file" "$newFilePath"
-
-		# 压缩逻辑
-		originalSize=$(du -k "$newFilePath" | cut -f1)
-		# 确保使用正确的命令格式
-		squoosh-cli --webp -d "$IMAGE_DIR/after" -- "$newFilePath"
-		compressedFile="$IMAGE_DIR/after/$(basename "$newFilePath" .jpg).webp"
-		if [ -f "$compressedFile" ]; then
-			compressedSize=$(du -k "$compressedFile" | cut -f1)
-			echo "Original: $newName, Size: ${originalSize}KB; Compressed: $(basename "$compressedFile"), Size: ${compressedSize}KB"
-		else
-			echo "Error: Compressed file not found - $compressedFile"
-		fi
-
     fi
 done
 
